e330f2d3af016ae3b36847e7031ee7da0999ff86
RomanasV <romanasvenckus@gmail.com>
2023-11-14 10:05:34 +0200

    attributes and classes

--- 2023-11-14-dom-manipulation/script.js
+++ 2023-11-14-dom-manipulation/script.js
@@ -27,10 +27,63 @@ newParagraph.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing e
 
 paragraph.after(newParagraph)
 
-console.dir(newParagraph)
-
 // Būdas pakeisti HTML elemento klases
 newParagraph.className = 'pirma-klase antra-klase'
-console.dir(newParagraph.className)
+console.log(newParagraph.className)
 
+// newParagraph.className = 'trecia-klase'
+// newParagraph.className = newParagraph.className + ' trecia-klase'
+newParagraph.className += ' trecia-klase'
+console.log(newParagraph.className)
 
+// Prideda klasę prie HTML elemento
+newParagraph.classList.add('ketvirta-klase')
+newParagraph.classList.add('penkta-klase', 'sesta-klase')
+
+// Pašalina klasę iš HTML elemento
+newParagraph.classList.remove('antra-klase')
+newParagraph.classList.remove('penkta-klase', 'sesta-klase')
+
+// Pakeičia klasę HTML elemente
+newParagraph.classList.replace('pirma-klase', 'antra-klase')
+
+// Prideda klasę prie HTML elemento, jeigu tokios klasės dar neturi ir pašalina jeigu tokią klasę jau turi
+newParagraph.classList.toggle('toggle-klase') // prideda
+newParagraph.classList.toggle('toggle-klase') // pašalina
+newParagraph.classList.toggle('toggle-klase') // prideda
+
+// Patikrina ar HTML elementas turi nurodytą klasę (grąžina true arba false)
+console.log(newParagraph.classList.contains('antra-klase'))
+console.log(newParagraph.classList.contains('pirma-klase'))
+console.log(newParagraph.classList.contains('antra'))
+
+// Keičia (arba prideda) HTML elemento ID
+newParagraph.id = 'paragrafo-id'
+console.log(newParagraph.id)
+
+const link = document.createElement('a')
+link.textContent = 'nuoroda'
+newParagraph.after(link)
+
+// link.href = 'https://www.google.com'
+// link.target = '_blank'
+link.setAttribute('href', 'https://www.google.com')
+link.setAttribute('target', '_blank')
+link.setAttribute('title', 'cia yra nuoroda')
+link.setAttribute('class', 'nuorodos-klase')
+link.setAttribute('class', 'antra-nuorodos-klase')
+link.setAttribute('id', 'nuorodos-id')
+
+console.log(link.getAttribute('id'))
+console.log(link.id)
+
+console.log(link.getAttribute('class'))
+link.removeAttribute('class')
+console.log(link.getAttribute('class'))
+
+const attrName = 'target'
+const attrValue = '_blank'
+
+link.setAttribute(attrName, attrValue)
+
+console.dir(link)
